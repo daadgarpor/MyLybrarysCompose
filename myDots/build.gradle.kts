c3e13b23f3c271f1c8bb6228c511813ef0909fa9
DANIEL GARCIA PORTILLO <daniel.garciap@elektra.com.mx>
2024-05-01 19:41:23 -0600

    update gradle

--- myDots/build.gradle.kts
+++ myDots/build.gradle.kts
@@ -53,7 +53,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.github.daadgarpor"
                 artifactId = "my-dots"
-                version = "1.0"
+                version = "4.0"
 
                 afterEvaluate {
                     from(components["release"])
